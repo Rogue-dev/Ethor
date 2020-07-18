c6b75507511f39a141a2c53a1cea94301b30cddd
Glitch (carpal-great-silk) <none>
2020-07-18 17:39:44 +0000

    ⛰💸 Updated with Glitch

--- functions/commands/pickup.js
+++ functions/commands/pickup.js
@@ -1,20 +1,30 @@
 /*global rooms currentRoom addText inventory*/
 function get(object) {
+  console.log(object)
   if (rooms[currentRoom].items[object] !== undefined) {
+    console.log("Not undefined.")
     if (rooms[currentRoom].items[object] === "available") {
-      if (!(object === "meat" && currentRoom === "trolls")) {
+      console.log("available")
+      if (!(!object === "meat" && currentRoom === "trolls")) {
+        console.log("Last if gate passed successfully.")
         inventory.push(
           Object.keys(rooms[currentRoom].items)[
             Object.keys(rooms[currentRoom].items).indexOf(object)
           ]
         );
+        console.log("pushed")
         addText("You have received a " + object + "!");
+        console.log("notification received.")
         rooms[currentRoom].items[object] === "unavailable";
+        console.log("marked unavailable.")
+      } else if (
+        rooms.trolls.enemies.troll1.status === "alive" ||
+        rooms.trolls.enemies.troll2.status === "alive" ||
+        rooms.trolls.enemies.troll3.status === "alive"
+      ) {
+        addText("Trolls are guarding the meat!");
       }
     } else {
-      if (rooms[currentRoom].enemies.troll1.status === "alive" || rooms[currentRoom].enemies.troll2.status === "alive"|| rooms[currentRoom].enemies.troll3.status === "alive") {
-        addText("Trolls are still guarding the meat!")
-      }
       addText("You have already picked up that item!");
     }
   } else {
--- functions/commands/talk.js
+++ functions/commands/talk.js
@@ -1,4 +1,4 @@
-/*global rooms currentRoom addText oldLadyQuest inventory*/
+/*global rooms currentRoom addText oldLadyQuest inventory experience checkExperience*/
 function talk(target) {
   var newTarget = target.splice(2, target.length - 1);
   console.log(newTarget);
@@ -8,12 +8,24 @@ function talk(target) {
   console.log(newTarget);
   if (rooms[currentRoom].people[newTarget] !== undefined) {
     addText(rooms[currentRoom].people[newTarget]);
-    if (newTarget === "strange old lady" && currentRoom === "lighthouse" && !inventory.includes("meat")) {
+    if (
+      newTarget === "strange old lady" &&
+      currentRoom === "lighthouse" &&
+      !inventory.includes("meat")
+    ) {
       oldLadyQuest();
-    } else if (newTarget === "strange old lady" && currentRoom === "lighthouse" && inventory.includes("meat")) {
-      inventory.splice(inventory.indexOf("meat"),1)
-      addText("Thank you for the meat...Here is your reward, as promised.")
-      addText("You have received a gold coin!")
+    } else if (
+      newTarget === "strange old lady" &&
+      currentRoom === "lighthouse" &&
+      inventory.includes("meat")
+    ) {
+      inventory.splice(inventory.indexOf("meat"), 1);
+      addText("Thank you for the meat...Here is your reward, as promised.");
+      addText("You have received 50 experience!");
+      for (var i = 0; i<10; i++) {
+      experience += 5;
+      checkExperience();
+      }
     }
   } else {
     addText("No target found by that name!");
