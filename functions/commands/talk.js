ee539fa81835320354ded3b25e118572ce24c56a
Glitch (carpal-great-silk) <none>
2020-07-19 17:47:17 +0000

    😄🎥 Updated with Glitch

--- functions/commands/talk.js
+++ functions/commands/talk.js
@@ -1,33 +1,36 @@
 /*global rooms currentRoom addText oldLadyQuest inventory experience checkExperience*/
 function talk(target) {
   var newTarget = target.splice(2, target.length - 1);
-  console.log(newTarget);
   newTarget = newTarget.join();
-  console.log(newTarget);
   newTarget = newTarget.replace(/,/g, " ");
-  console.log(newTarget);
-  if (rooms[currentRoom].people[newTarget] !== undefined) {
-    addText(rooms[currentRoom].people[newTarget]);
-    if (
-      newTarget === "strange old lady" &&
-      currentRoom === "lighthouse" &&
-      !inventory.includes("meat")
-    ) {
-      oldLadyQuest();
-    } else if (
-      newTarget === "strange old lady" &&
-      currentRoom === "lighthouse" &&
-      inventory.includes("meat")
-    ) {
-      inventory.splice(inventory.indexOf("meat"), 1);
-      addText("Thank you for the meat...Here is your reward, as promised.");
-      addText("You have received 50 experience!");
-      for (var i = 0; i<10; i++) {
-      experience += 5;
-      checkExperience();
+  if (rooms[currentRoom].people !== undefined) {
+    if (rooms[currentRoom].people[newTarget] !== undefined) {
+      addText(rooms[currentRoom].people[newTarget]);
+      if (
+        newTarget === "strange old lady" &&
+        currentRoom === "lighthouse" &&
+        !inventory.includes("meat")
+      ) {
+        oldLadyQuest();
+      } else if (
+        newTarget === "strange old lady" &&
+        currentRoom === "lighthouse" &&
+        inventory.includes("meat")
+      ) {
+        inventory.splice(inventory.indexOf("meat"), 1);
+        addText("Thank you for the meat...Here is your reward, as promised.");
+        addText("You have received 50 experience!");
+        for (var i = 0; i < 10; i++) {
+          experience += 5;
+          checkExperience();
+        }
       }
+    } else if (Object.keys(rooms[currentRoom].enemies).includes(target)) {
+      addText("No target found by that name!");
+    } else {
+      addText("You cannot talk to that person, as they are an enemy!");
     }
-  } else if (!Object.keys(rooms[currentRoom].enemies).includes(target)){
+  } else if (Object.keys(rooms[currentRoom].enemies).includes(target)) {
     addText("No target found by that name!");
   } else {
     addText("You cannot talk to that person, as they are an enemy!");
